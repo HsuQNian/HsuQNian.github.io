0876dfb51b61d49a47391eb27d38ab2b070b3e13
HsuQnian <57381605+HsuQNian@users.noreply.github.com>
2023-04-08 03:22:19 +0800

    load

--- pages/Project/Notice/index.vue
+++ pages/Project/Notice/index.vue
@@ -37,7 +37,6 @@ const scroll = () => {
 <template>
   <div class="Notice" @scroll="scroll(event)">
     {{ height }}
-    <Back :router="'/project'" :height="height" />
     <div class="introduce" style="flex: 1">
       <h1 style="font-size: 4rem; margin: 1.2rem">{{ AppName }}</h1>
       <p>当前版本 : {{ version }}</p>

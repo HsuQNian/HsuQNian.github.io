8588bd45e1dbe0b679c2c077b410e3e211cb6fb6
HsuQnian <57381605+HsuQNian@users.noreply.github.com>
2023-04-17 17:43:35 +0800

    load

--- app.vue
+++ app.vue
@@ -1,12 +1,6 @@
 <script setup>
 const route = useRoute();
 const onReady = ref(false);
-const backMusic = ref(null);
-const Musics = Object.keys(import.meta.globEager("./**/*.mp3"));
-const Move = Musics[Math.floor(Math.random() * Musics.length)];
-Musics.splice(Musics.indexOf(Move), 1);
-Musics.unshift(Move);
-const Music = ref(Musics[0].replace("/public", ""));
 useHead({
   titleTemplate: () => {
     return `${
@@ -20,25 +14,10 @@ useHead({
   },
 });
 onMounted(() => {
-  document.readyState == "complete"
-    ? (onReady.value = true) && backMusic.value.play()
-    : "";
+  document.readyState == "complete" ? (onReady.value = true) : "";
 });
-const next = () => {
-  Music.value = Musics[
-    Musics.indexOf(Music.value.replace("/", "/public/")) + 1 > Musics.length - 1
-      ? 0
-      : Musics.indexOf(Music.value.replace("/", "/public/")) + 1
-  ].replace("/public", "");
-  console.log(Music.value.replace("/", "/public"));
-  backMusic.value.load();
-  backMusic.value.play();
-};
 </script>
 <template>
-  <audio ref="backMusic" preload="metadata" controls @ended="next">
-    <source :src="Music" type="audio/mpeg" />
-  </audio>
   <transition name="Loading" mode="out-in">
     <Loading v-if="!onReady" />
     <div v-else>
